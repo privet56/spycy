25d79c352e6f551129a1ceb895a39f19046d7eb1
privet <privet56@hotmail.com>
2018-09-19 14:53:54 +0200

    seq2seq call

--- talkFellow/talkfellowCall.bat
+++ talkFellow/talkfellowCall.bat
@@ -1,2 +1,2 @@
-python talkfellow.py --mode=test --source=gutenberg --datadir=data --bookids=2229,12108,23134,53689,22517,52856
-rem python talkfellow.py --mode=train --source=gutenberg --datadir=data --bookids=2229,12108,23134,53689,22517,52856
+rem python talkfellow.py --mode=test --source=gutenberg --datadir=data --bookids=2229,12108,23134,53689,22517,52856
+python talkfellow.py --mode=train --source=gutenberg --datadir=data --bookids=2229,12108,23134,53689,22517,52856,50681,55914,52985,47318,53459,48425,52986,51223,7810,417,39553,21158
